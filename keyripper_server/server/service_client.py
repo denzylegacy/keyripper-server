b36c87779a352e2f87b4cb7bd0fdf7466c2a0716
Denzy Legacy <alissondin.developer@gmail.com>
2024-09-22 15:58:21 -0300

    Header removal

--- keyripper_server/server/service_client.py
+++ keyripper_server/server/service_client.py
@@ -12,16 +12,16 @@ class SimpleClient:
         self.url = "http://127.0.0.1:8000/request"
         self.token = os.getenv("API_AUTH_TOKEN", "")
 
-    def simple_client_post(self, parameter: str = None):
+    def simple_client_post(self):
         payload = {
             'message': 'This is a confirmation message.'
         }
 
-        headers = {
-            'Authorization': f'Bearer {self.token}'
-        }
+        # headers = {
+        #     'Authorization': f'Bearer {self.token}'
+        # }
 
-        response = requests.post(self.url, json=payload, headers=headers)
+        response = requests.post(self.url, json=payload)  # headers=headers
         return response
 
 
