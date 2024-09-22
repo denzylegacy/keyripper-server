a064ba7726f368c4bbbe3e05444e0e788bb5d63a
Denzy Legacy <alissondin.developer@gmail.com>
2024-09-22 15:54:00 -0300

    Middleware upodate

--- keyripper_server/server/server.py
+++ keyripper_server/server/server.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .routes import router
 
@@ -9,6 +10,14 @@ app = FastAPI(
         404: {'error': True, 'message': 'Not Found', 'data': []},
         500: {'error': True, 'message': 'Internal Server Error', 'data': []},
     },
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(router)
